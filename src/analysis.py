f43b31d16372e26cdd8cd2cab0f647e9ff8c3e41
i30101 <andrewkim229@gmail.com>
2025-06-05 09:07:08 -0400

    analysis

---
+++ src/analysis.py
@@ -0,0 +1,35 @@
+"""
+Andrew Kim
+
+17 June 2025
+
+Version 0.9.0
+
+Analysis module for CrystalEyes
+TODO lots of improvements...
+"""
+
+
+import cv2
+import math
+import numpy as np
+# TODO find a way to not use pytesseract
+# import pytesseract
+from datetime import datetime
+from cellpose import models, io
+
+
+class Analysis:
+    # constants
+    DEFAULT_PX = 808.669
+    DEFAULT_UM = 100
+    DEFAULT_SCALE = round(DEFAULT_UM / DEFAULT_PX, 5)
+
+    # cropping defaults
+    MAX_Y = 1536
+    MAX_X = 2048
+
+    def __init(self):
+        self.scale = Analysis.DEFAULT_SCALE
+
+        
